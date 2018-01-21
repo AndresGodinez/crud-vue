3fa2167c3592064f10fec144a4ac99bc4d031b37
Andres Vagrant <ing.a.godinez@gmail.com>
2018-01-21 23:12:41 +0000

    configuracion inicial de vista

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,6 @@
 //var urlUsers = 'https://randomuser.me/api/?results=10';
 // var urlUsers = 'https://jsonplaceholder.typicode.com/users';
 new Vue({
+	el:'#main',
+	
 });
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,6 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dash-board');
 });
+Route::resource('tasks', 'taskController', ['except'=>'show']);
